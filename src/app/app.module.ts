be39987c2a45acb04dbb0e0d126a834734d135fe
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-19 00:32:29 -0600

    Estructura del template listo (Sin rutas)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { VistaHomeComponent } from './vistas/vista-home/vista-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VistaHomeComponent
   ],
   imports: [
     BrowserModule,
