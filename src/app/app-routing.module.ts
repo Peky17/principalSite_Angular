c297d4033d117d0fb7827082af11d94c078aed3c
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-19 14:21:54 -0600

    Se crearon las rutas correspondientes a la principal page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+import { HabitacionesComponent } from "./vistas/habitaciones/habitaciones.component";
+import { LoginComponent } from "./vistas/login/login.component";
+import { RegistrarUsuarioComponent } from "./vistas/registrar-usuario/registrar-usuario.component";
+import { VistaAboutComponent } from "./vistas/vista-about/vista-about.component";
+import { VistaContactoComponent } from "./vistas/vista-contacto/vista-contacto.component";
+import { VistaHomeComponent } from "./vistas/vista-home/vista-home.component";
 
-const routes: Routes = [];
+const app_routes: Routes= [
+  { path: '', component: VistaHomeComponent },
+  { path: 'about', component: VistaAboutComponent },
+  { path: 'contacto', component: VistaContactoComponent },
+  { path: 'habitaciones', component: HabitacionesComponent },
+  { path: 'registrar', component: RegistrarUsuarioComponent },
+  { path: 'ingresar', component: LoginComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+@NgModule ({
+  imports: [
+    RouterModule.forRoot(app_routes)
+  ], exports: [
+      RouterModule
+  ] 
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+  
+}
