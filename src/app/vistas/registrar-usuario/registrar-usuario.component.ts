9d5b9a1c42f3be1d7a6895c80c4637ed8fd9cc81
Enrique Bernardo Gonzalez Vazquez <ebgvdeveloper@gmail.com>
2022-11-20 12:33:40 -0600

    se obtuvieron los valores para registrar un usuario

--- src/app/vistas/registrar-usuario/registrar-usuario.component.ts
+++ src/app/vistas/registrar-usuario/registrar-usuario.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './registrar-usuario.component.html',
   styleUrls: ['./registrar-usuario.component.css']
 })
+
 export class RegistrarUsuarioComponent {
+  nombre: string;
+  apellidos: string;
+  telefono: string;
+  email: string;
+  direccion: string;
+  password: string;
+  confirmarPassword: string;
+
+  constructor() {
+    this.nombre = "";
+    this.apellidos = "";
+    this.telefono = "";
+    this.email = "";
+    this.direccion = "";
+    this.password = "";
+    this.confirmarPassword = "";
+  }
+
+  registrar(){
+    console.log(this.nombre);
+    console.log(this.apellidos);
+    console.log(this.telefono);
+    console.log(this.email);
+    console.log(this.direccion);
+    console.log(this.password);
+    console.log(this.confirmarPassword);
+  }
 
 }
